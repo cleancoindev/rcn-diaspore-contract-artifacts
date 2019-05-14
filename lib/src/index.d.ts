1cf6aa0fd889d898f00bc368868d62d571c47798
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-05-14 15:34:35 -0300

    Auto release 1.0.7

---
+++ lib/src/index.d.ts
@@ -0,0 +1,19 @@
+import * as Cosigner from '../artifacts/Cosigner.json';
+import * as DebtEngine from '../artifacts/DebtEngine.json';
+import * as ERC165 from '../artifacts/ERC165.json';
+import * as ERC721Base from '../artifacts/ERC721Base.json';
+import * as IERC20 from '../artifacts/IERC20.json';
+import * as IERC165 from '../artifacts/IERC165.json';
+import * as IERC173 from '../artifacts/IERC173.json';
+import * as InstallmentsModel from '../artifacts/InstallmentsModel.json';
+import * as LoanApprover from '../artifacts/LoanApprover.json';
+import * as LoanManager from '../artifacts/LoanManager.json';
+import * as Model from '../artifacts/Model.json';
+import * as ModelDescriptor from '../artifacts/ModelDescriptor.json';
+import * as Oracle from '../artifacts/Oracle.json';
+import * as Registry from '../artifacts/Registry.json';
+import * as OracleAdapter from '../artifacts/OracleAdapter.json';
+import * as Ownable from '../artifacts/Ownable.json';
+import * as RateOracle from '../artifacts/RateOracle.json';
+export { Cosigner, DebtEngine, ERC165, ERC721Base, IERC20, IERC165, IERC173, InstallmentsModel, LoanApprover, LoanManager, Model, ModelDescriptor, Oracle, OracleAdapter, Ownable, Registry, RateOracle };
+//# sourceMappingURL=index.d.ts.map
